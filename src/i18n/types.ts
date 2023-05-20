3b38695bff00c2b3e4d1eb8a3703051e32a19040
Jakob Mick <jakobmick@live.de>
2023-05-21 01:20:35 +0200

    refactor: format

--- src/i18n/types.ts
+++ src/i18n/types.ts
@@ -4,24 +4,24 @@ import type englishTranslations from "@i18n/en";
 import type englishNamespaces from "@i18n/en/namespaces";
 
 export namespace I18n {
-	export type Namespaces =
-		| keyof typeof germanNamespaces
-		| keyof typeof englishNamespaces;
-	export type Descriptions =
-		| keyof typeof germanTranslations.descriptions
-		| keyof typeof englishTranslations.descriptions;
-	export type Links =
-		| keyof typeof germanTranslations.links
-		| keyof typeof englishTranslations.links;
 	export type Routes =
 		| keyof typeof germanTranslations.routes
 		| keyof typeof englishTranslations.routes;
+	export type Links =
+		| keyof typeof germanTranslations.links
+		| keyof typeof englishTranslations.links;
 	export type Titles =
 		| keyof typeof germanTranslations.titles
 		| keyof typeof englishTranslations.titles;
+	export type Descriptions =
+		| keyof typeof germanTranslations.descriptions
+		| keyof typeof englishTranslations.descriptions;
 	export type Translations =
 		| keyof typeof germanTranslations.translations
 		| keyof typeof englishTranslations.translations;
+	export type Namespaces =
+		| keyof typeof germanNamespaces
+		| keyof typeof englishNamespaces;
 }
 
 export type DeepestKeys<T> = T extends string
